53d88cfe6fdac22be818586a217ac48e3a3f3642
Shane Charles <shanecharles@burningicesolutions.com>
2020-11-16 17:22:06 -0600

    Add F# directions reduction solution.

---
+++ fsharp/directions_reduction/directions_reduction.fsx
@@ -0,0 +1,14 @@
+let isOpposite =
+    function
+    | "NORTH", "SOUTH"
+    | "SOUTH", "NORTH"
+    | "EAST", "WEST"
+    | "WEST", "EAST" -> true
+    | _ -> false
+
+let folder state dir =
+    match state with
+    | x :: tail when isOpposite (x, dir) -> tail
+    | _ -> dir :: state
+
+let reduceDir ls = ls |> Seq.fold folder [] |> List.rev
